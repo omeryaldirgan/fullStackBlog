4d75554f945704c1a15ae26a491e1a2d0e21414b
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-01-10 22:42:49 +0300

    Mongo db bağlantısı oluşturuldu

---
+++ server/index.js
@@ -0,0 +1,30 @@
+
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+   res.send("Musmutlu yıllar !!");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+.connect(CONNECTION_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+})
+.then(() => {
+   app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
+})
+.catch((error) => {
+   console.error(error.message);
+});
