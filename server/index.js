fd5f3cce942046e0fca93399056cda63be6247ed
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-01-17 18:32:39 +0300

    Routes-Model-Controllers yapısı oluşturuldu.

--- server/index.js
+++ server/index.js
@@ -2,23 +2,31 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 import cors from "cors";
+import postRoutes from "./routes/posts.js"
+
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-   res.send("Musmutlu yıllar !!");
+   //res.send("Musmutlu yıllar !!1");
+   res.json({
+      author:'Omer yaldirgan',
+      message:'Mutlu Yıllar:)'
+   })
 });
 
+app.use("/posts",postRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 mongoose
-.connect(CONNECTION_URL, {
+.connect(process.env.CONNECTION_URL, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
 })
