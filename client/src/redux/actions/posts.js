08bacb139136611b3c6afff6d00bf61e16b01b62
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-01-22 22:03:24 +0300

    genel düzenleme

--- client/src/redux/actions/posts.js
+++ client/src/redux/actions/posts.js
@@ -1,5 +1,5 @@
  import * as types from './types'
-
+//Action: Bir javascript objesidir. Store’da hangi state’in değişeceği bilgisini(type) ve state verisini taşır(payload)
 
 export const fetchPosts=()=>{
    return {
